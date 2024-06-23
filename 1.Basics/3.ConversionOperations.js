f3d39b1a97248f858496ae2fcf5d6d33f01b8279
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-06-23 11:43:57 +0000

    Conversion Operations

---
+++ 1.Basics/3.ConversionOperations.js
@@ -0,0 +1,32 @@
+let score = "21new"
+// let score = null
+// let score = undefined
+// let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+32new = not a number NaN
+true/false = 1/0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// " " = false
+// "Harsh" = true
+
+let randomNumber = 21
+
+let newNumber = String(randomNumber)
+console.log(newNumber)
+console.log(typeof newNumber)
