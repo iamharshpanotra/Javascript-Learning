278f01c36142b9e6212f2bf3b6d62f2e7748cc73
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-06-23 16:01:06 +0530

    Add files via upload

---
+++ 1.Basics/2.datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // it treats the Javascript code as of newer version.
+
+//alert(1 + 5) // this will not give any output as we are using nodejs, not browser.
+
+console.log(2 +
+        3) // code readibility is the first to be made sure of as is written below.
+
+console.log("HS Panotra")
+
+let name = "Harsh"
+let age = 21
+let isLoggedIn = false
+let state;
+
+/* number => 2 to the power 53
+bigint
+string = " "
+boolean = true/false
+null = standalone value
+undefined = 
+symbol = unique
+*/
+
+//object
+
+console.log(typeof "harsh"); //string
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
