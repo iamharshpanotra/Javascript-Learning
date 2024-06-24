46e6bb3f8cf776b76180ee11a651fe920c979ce9
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-06-24 19:25:58 +0000

    Comparison Operators

---
+++ 1.Basics/4.Comparison.js
@@ -0,0 +1,23 @@
+// console.log(3>4)
+// console.log(3<4)
+// console.log(3>=4)
+// console.log(3<=4)
+// console.log(3==4)
+// console.log(3!=4)
+
+// Before comparing make sure that the data type is same of the comparing data.
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// The reason is that an equality check == and comparison > < >= <= work differently. 
+//Comparisons convert null to a number, treating it as 0.
+// That's why null >= 0 is true and null > 0 is false.
+
+// console.log(undefined == 0);
+
+
+// === type check
+
+console.log("2" === 2);
