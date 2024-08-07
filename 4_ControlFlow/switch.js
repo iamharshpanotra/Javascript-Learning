b14bd4303fe2d1494624cffbd2bd53cdd91f80f2
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-08-07 04:42:38 +0000

    Switch in Javascript

---
+++ 4_ControlFlow/switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         //break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("No value is capable");
+//         break;
+// }
+
+
+const month = "march";
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        //break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("No value is capable");
+        break;
+}
