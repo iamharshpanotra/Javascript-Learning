8515331876a115a3ea475d2f176f142200cebc77
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-08-13 05:51:15 +0000

    Iterations in Javascript

---
+++ 5_Iterations/Two.js
@@ -0,0 +1,19 @@
+// let i = 0;
+// while (i <= 21) {
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// let ArrayOne = ["one", "two", "three"];
+// let arr = 0;
+// // console.log(ArrayOne.length);
+// while (arr < ArrayOne.length) {
+//     console.log(`Value is ${ArrayOne[arr]}`);
+//     arr = arr + 1;
+// }
+
+let seats = 0;
+do {
+    console.log(`Score is ${seats}`);
+    seats++;
+} while (seats <= 272);
