93e8b73c8bb7d992c801828380b8d9d740c948a0
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-10-17 12:56:27 +0530

    Update truthy.js

--- 4_ControlFlow/truthy.js
+++ 4_ControlFlow/truthy.js
@@ -19,7 +19,7 @@ if (userEmail) {
 // if (userEmail.length === 0) {
 //     console.log("Array is Empty");
 // }
-
+ 
 const emptyObj = {};
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
