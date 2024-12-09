d6f943c3b7580315679bd722102c234b39b88965
Harsh Sharma <sharmaharsh506325@gmail.com>
2024-12-09 10:53:00 +0000

    Project 2: BMI Calculator

---
+++ 7.Projects/Project 2 - BMI Calculator/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form') 
+
+    //this usecase will give empty value 
+
+    //const height = parseInt(document.querySelector('#height').value) 
+
+form.addEventListener('submit', function(e) { 
+
+    e.preventDefault(); 
+
+    const height = parseInt(document.querySelector('#height').value); 
+
+    const weight = parseInt(document.querySelector('#weight').value); 
+
+    const results = document.querySelector('#results'); 
+
+    if (height === '' || height < 0 || isNaN(height)) { 
+
+        results.innerHTML = `Please give a valid height`; 
+
+    } else if (weight === '' || weight < 0 || isNaN(weight)) { 
+
+        results.innerHTML = `Please give a valid weight ${weight}`; 
+
+    } else { 
+
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2); 
+
+        //Displaying Result 
+
+        results.innerHTML = `<span>${bmi}</span>`; 
+
+    } 
+
+}); 
